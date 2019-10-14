32f90299caf343c1280b08e6f1d3e935b3bdd150
smismi <smirnov.ru@gmail.com>
2019-10-14 23:40:24 +0300

    BRANCH_01 time measure

---
+++ timeMeasuring.js
@@ -0,0 +1,31 @@
+const start = process.hrtime();
+
+const arr = [];
+
+for (let i = 0; i< 5000; i++) {
+	arr.push({
+		a: ~~(Math.random() * 5000)
+	})
+}
+
+
+const arr2 = arr.reduce((acc, item) => {
+	return {...acc, [`id_${item['a']}`]: `title_${item['a'] + 'testtesttest'}`}
+}, {});
+
+// const arr2 = arr.reduce((acc, item) => {
+// 	acc[`id_${item['a']}`] = `title_${item['a'] + 'testtesttest'}`
+// 	return acc
+// }, {});
+
+
+console.log(arr2);
+const end = process.hrtime(start);
+console.info('Execution time (hr): %ds %dms', end[0], end[1] / 1000000);
+
+
+// setTimeout(() => {
+// }, 500);
+
+
+
